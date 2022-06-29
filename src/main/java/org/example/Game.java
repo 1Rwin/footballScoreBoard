9bb8848857d88824174a430031b80558df3927b1
fparadow <filip.paradowski@dbschenker.com>
2022-06-29 12:48:08 +0200

    ADD equals and hashcode method for class Game and Team

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -23,4 +23,17 @@ public class Game {
         this.awayTeamScore = 0;
         this.isFinished = false;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game)) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id) && Objects.equals(homeTeam, game.homeTeam) && Objects.equals(awayTeam, game.awayTeam) && Objects.equals(homeTeamScore, game.homeTeamScore) && Objects.equals(awayTeamScore, game.awayTeamScore) && Objects.equals(isFinished, game.isFinished);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, homeTeam, awayTeam, homeTeamScore, awayTeamScore, isFinished);
+    }
 }
--- src/main/java/org/example/Team.java
+++ src/main/java/org/example/Team.java
@@ -1,9 +1,9 @@
 package org.example;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Team {
-
     UUID id;
     String name;
 
@@ -14,5 +14,18 @@ public class Team {
     private Team(String name) {
         this.id = UUID.randomUUID();
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Team)) return false;
+        Team team = (Team) o;
+        return Objects.equals(id, team.id) && Objects.equals(name, team.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
